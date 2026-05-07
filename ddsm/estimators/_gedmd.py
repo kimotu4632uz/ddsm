f5c19df9db90527a8ff72f39746be2f87b01724e
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2026-05-07 05:37:38 +0000

    feat: add 'gEDMD' class

---
+++ ddsm/estimators/_gedmd.py
@@ -0,0 +1,139 @@
+from __future__ import annotations
+import warnings
+from typing import Any, Literal
+import numpy as np
+from scipy import linalg
+from sklearn.linear_model import Lasso, Ridge
+from sklearn.utils.validation import validate_data, check_is_fitted
+from ..base._dicts import BaseDict
+from ..base._estimators import DDSMBaseEstimator
+from ..dicts._dicts import IdentityDict
+
+class gEDMD(DDSMBaseEstimator):
+    psi_: BaseDict
+    L_: np.ndarray
+    _psi_cls: type[BaseDict]
+    _psi_kwargs: dict[str, Any]
+    _reg: Literal['none', 'lasso', 'ridge']
+    _reg_kwargs: dict[str, Any]
+    _is_psi_fitted: bool
+    _y_ndim_1d: bool
+    _y_features: int
+    def __init__(
+        self,
+        psi_cls: type[BaseDict] = IdentityDict,
+        psi_kwargs: dict[str, Any] | None = None,
+        reg:Literal['none', 'lasso', 'ridge'] = 'none',
+        reg_kwargs: dict[str, Any] | None = None
+    ) -> None:
+        super(gEDMD, self).__init__()
+        self.psi_cls = psi_cls
+        self.psi_kwargs = psi_kwargs
+        self.reg = reg
+        self.reg_kwargs = reg_kwargs
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> gEDMD:
+        X, y = validate_data(self, X, y, reset=True, multi_output=True)
+        self._y_ndim_1d = (y.ndim == 1)
+        if self._y_ndim_1d:
+            y = y[:, np.newaxis]
+
+        self._y_features = y.shape[1]
+        if X.shape[1] != y.shape[1]:
+            warnings.warn(f'Number of features in X ({X.shape[1]}) does not match number of features in y ({y.shape[1]}). This may indicate that the data is not properly aligned. Ensure that each row of X corresponds to the same time step as the corresponding row of y.')
+            _y = np.zeros((y.shape[0], X.shape[1]), dtype=y.dtype)
+            col = min(X.shape[1], y.shape[1])
+            _y[:, :col] = y[:, :col]
+            y = _y
+        self._fit_psi(X)
+        PsiX = self.psi_.lift(X)
+        dPsiX = np.sum(self.psi_.diff(X) * y[:, np.newaxis, :], axis=2)
+        if self.reg == 'none':
+            G = PsiX.T @ PsiX
+            A = PsiX.T @ dPsiX
+            self.L_ = linalg.pinv(G) @ A
+        elif self.reg == 'lasso' or self.reg == 'ridge':
+            reg_kwargs = self.reg_kwargs.copy() if self.reg_kwargs is not None else {}
+            reg_kwargs.setdefault('fit_intercept', False)
+            reg = Lasso(**reg_kwargs) if self.reg == 'lasso' else Ridge(**reg_kwargs)
+            reg.fit(PsiX, dPsiX)
+            self.L_ = np.atleast_2d(reg.coef_).T
+
+        else:
+            raise ValueError(f'Invalid regularization type: {self.reg}. Must be one of "none", "lasso", or "ridge".')
+        return self
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        check_is_fitted(self, 'L_')
+        X = validate_data(self, X, reset=False)
+        PsiX = self.psi_.lift(X)
+        dPsiX = PsiX @ self.L_
+        y = self.psi_.reconstruct(dPsiX)
+        if hasattr(self, '_y_features') and y.shape[1] != self._y_features:
+            y = y[:, :self._y_features]
+        if hasattr(self, '_y_ndim_1d') and self._y_ndim_1d:
+            y = y.ravel()
+        return y
+
+    def _fit_psi(self, X: np.ndarray) -> None:
+        if not hasattr(self, 'psi_') or not self._is_psi_fitted:
+            self.psi_ = self.psi_cls(**self.psi_kwargs.copy() if self.psi_kwargs is not None else {})
+            self.psi_.fit(X)
+            self._is_psi_fitted = True
+
+    @property
+    def right_L(self) -> np.ndarray:
+        check_is_fitted(self, 'L_')
+        return self.L_
+
+    @property
+    def left_L(self) -> np.ndarray:
+        return self.right_L.conj().T
+
+    def right_K(self, dt: float) -> np.ndarray:
+        return linalg.expm(self.right_L * dt)
+
+    def left_K(self, dt: float) -> np.ndarray:
+        return self.right_K(dt).conj().T
+
+    @property
+    def psi_cls(self) -> type[BaseDict]:
+        return self._psi_cls
+
+    @psi_cls.setter
+    def psi_cls(self, value: type[BaseDict]) -> None:
+        self._psi_cls = value
+        self._is_psi_fitted = False
+
+    @property
+    def psi_kwargs(self) -> dict[str, Any]:
+        return self._psi_kwargs
+
+    @psi_kwargs.setter
+    def psi_kwargs(self, value: dict[str, Any] | None) -> None:
+        self._psi_kwargs = value
+        self._is_psi_fitted = False
+
+    @property
+    def reg(self) -> Literal['none', 'lasso', 'ridge']:
+        return self._reg
+
+    @reg.setter
+    def reg(self, value: Literal['none', 'lasso', 'ridge']) -> None:
+        self._reg = value
+
+    @property
+    def reg_kwargs(self) -> dict[str, Any]:
+        return self._reg_kwargs
+
+    @reg_kwargs.setter
+    def reg_kwargs(self, value: dict[str, Any] | None) -> None:
+        self._reg_kwargs = value
+
+    @property
+    def is_psi_fitted(self) -> bool:
+        return self._is_psi_fitted
+
+    @is_psi_fitted.setter
+    def is_psi_fitted(self, value: bool) -> None:
+        self._is_psi_fitted = value
